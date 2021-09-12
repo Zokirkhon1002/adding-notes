ab0e8b219f377c6b2581bec3db5944ba07f43475
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-09-13 02:42:48 +0900

    adding some features

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let add_btn = document.getElementById("add"),
 if(notes) {
   notes.forEach((note) => addNewNote(note));
 }
-
-add_btn.addEventListener("click", () => addNewNote());
+add_btn.addEventListener("click", () => {
+  addNewNote();
+});
 
 function addNewNote(text = "") {
   const note = document.createElement("div");
@@ -13,6 +14,7 @@ function addNewNote(text = "") {
 
   note.innerHTML = `
 <div class="tools">
+    <input class="theme" value="Theme..."></input>
     <button class="edit"><i class="fas fa-edit"></i></button>
     <button class="delete"><i class="fas fa-trash-alt"></i></button>
 </div>
@@ -24,7 +26,9 @@ function addNewNote(text = "") {
   let edit_btn = note.querySelector(".edit"),
     del_btn = note.querySelector(".delete"),
     main = note.querySelector(".main"),
-    text_area = note.querySelector("textarea");
+    text_area = note.querySelector("textarea"),
+    theme_edit = note.querySelector(".theme");
+
 
   text_area.value = text;
   main.innerHTML = marked(text);
@@ -40,22 +44,32 @@ function addNewNote(text = "") {
     text_area.classList.toggle("hidden");
   });
 
+
   text_area.addEventListener("input", (e) => {
     const { value } = e.target;
     main.innerHTML = marked(value);
 
     updateLS();
   });
+
+
   document.body.appendChild(note);
 }
 
 function updateLS() {
   const notesText = document.querySelectorAll("textarea");
+  const themeQiymat = document.querySelectorAll(".theme");
+
 
   const notes = [];
+  const themes = [];
+
 
   notesText.forEach(note => notes.push(note.value));
+  themeQiymat.forEach(theme => themes.push(theme.value));
+
 
 
   localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("Themes", JSON.stringify(themes));
 }
